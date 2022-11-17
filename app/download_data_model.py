41cf2ee7942efb9eaf3db2d1834323d12cb10256
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 19:56:08 +0000

    Adding tarfile member sanitization to extractall()

--- app/download_data_model.py
+++ app/download_data_model.py
@@ -37,9 +37,47 @@ class CloudStorage:
 
         # extract them
         with tarfile.open('data.tar.gz') as tfile:
-            tfile.extractall('./')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tfile, "./")
         with tarfile.open('model.tar.gz') as tfile:
-            tfile.extractall('./')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tfile, "./")
 
         # delete the compressed ones.
         os.remove('data.tar.gz')
